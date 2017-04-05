b9c485e42953d9daf0ed538ce7fe83bff53df572
Stephen Mayeux <stephenmayeux@gmail.com>
2017-04-04 22:16:44 -0500

    initial commit

---
+++ socketRoutes.js
@@ -0,0 +1,15 @@
+var counter = 0;
+
+module.exports = (socket) => {
+
+  socket.emit('init', {
+    counter
+  });
+
+  socket.on('incrementCounter', (data) => {
+    counter = data;
+    socket.broadcast.emit('incrementCounter', {
+      counter
+    });
+  });
+}
--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,50 @@
 import React, { Component } from 'react';
+const socket = io.connect();
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    }
+
+    this.onInit = this.onInit.bind(this);
+    this.handleIncrement = this.handleIncrement.bind(this);
+    this.incrementCounter = this.incrementCounter.bind(this);
+  }
+
+  componentDidMount() {
+    socket.on('init', this.onInit);
+    socket.on('incrementCounter', this.incrementCounter)
+  }
+
+  onInit({ counter }) {
+    this.setState({ counter });
+  }
+
+  handleIncrement() {
+    let { counter } = this.state;
+    counter++;
+    this.setState({ counter });
+    socket.emit('incrementCounter', counter);
+  }
+
+  incrementCounter({ counter }) {
+    this.setState({ counter });
+  }
+
   render() {
     return (
-      <div>React simple starter</div>
+      <div className="container">
+        <h1>The Counter</h1>
+        <h2>{this.state.counter}</h2>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-primary"
+        >
+          Increment
+        </button>
+      </div>
     );
   }
 }
